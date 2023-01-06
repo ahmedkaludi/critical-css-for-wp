2e980444c747076f2a97d4adaa9daab3cc7b1cfe
Akshay Wali <akshaywali@live.in>
2023-01-06 12:12:32 +0530

    Quick fix for Undefined array key "exclude_delay_js"

--- admin/settings.php
+++ admin/settings.php
@@ -391,7 +391,8 @@ function critical_css_get_tab( $default = '', $available = array() ) {
 function critical_css_defaults(){
     $defaults = array(
        'ccfwp_on_home'      => 1,
-       'ccfwp_on_cp_type'   => array( 'post' => 1 )
+       'ccfwp_on_cp_type'   => array( 'post' => 1 ),
+       'exclude_delay_js'   =>'',
     );        
     $settings = get_option( 'ccfwp_settings', $defaults );
     return $settings;
